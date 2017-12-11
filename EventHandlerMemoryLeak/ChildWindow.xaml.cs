ccb42c7b41e6ea7bbc0fc5ccf069ced202b74739
Borislav Ivanov <bsivanov@gmail.com>
2017-12-11 19:09:51 +0200

    Add event handler memory leak demo

---
+++ EventHandlerMemoryLeak/ChildWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EventHandlerMemoryLeak
+{
+    /// <summary>
+    /// Interaction logic for ChildWindow.xaml
+    /// </summary>
+    public partial class ChildWindow : Window
+    {
+        public ChildWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal void SetPerson(Person person)
+        {
+            person.Sang += Person_Sang;
+            this.DataContext = person;
+        }
+        
+        private void Person_Sang(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yeah!");
+        }
+    }
+}
---
+++ EventHandlerMemoryLeak/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventHandlerMemoryLeak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            person = new Person("Djordjano");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ChildWindow childWindow = new ChildWindow();
+            childWindow.SetPerson(person);
+            childWindow.ShowDialog();
+        }
+    }
+}
