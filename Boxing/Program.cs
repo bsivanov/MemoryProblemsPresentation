31174dcf22c22fa1225f4fb35c94ca42b312cefa
Borislav Ivanov <bsivanov@gmail.com>
2017-12-11 22:22:45 +0200

    Add boxing demo

---
+++ Boxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Trace.WriteLine(string.Format("i = {0}", i.ToString()));
+            }
+        }
+    }
+}
