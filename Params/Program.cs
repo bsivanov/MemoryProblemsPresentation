f7b24631b1ac6d77879d41fc929ef8d92ee1d14c
Borislav Ivanov <bsivanov@gmail.com>
2017-12-11 23:32:01 +0200

    Add params demo

---
+++ Params/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                Log("test", i);
+            }
+        }
+
+        private static void Log(string message, params object[] arguments)
+        {
+            Console.WriteLine(message);
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                Console.WriteLine(arguments[i].ToString());
+            }
+        }
+    }
+}
