02aa175116ecd4cd6ce271e6a7614329725f5852
Borislav Ivanov <bsivanov@gmail.com>
2017-12-11 22:46:45 +0200

    Add BechmarkDotNet demo

---
+++ BenchmarkDemo/TestBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkDemo
+{
+    public class TestBenchmark
+    {
+        [Benchmark]
+        public void StringFormatWithString()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Trace.WriteLine(string.Format("i = {0}", i.ToString()));
+            }
+        }
+
+        [Benchmark]
+        public void StringFormatWithInt()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Trace.WriteLine(string.Format("i = {0}", i));
+            }
+        }
+    }
+}
